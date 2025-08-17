4e8e70c2f045e1cef8585037f6e42b13f1f17581
Eduardo Alexis Gonzalez Sotelo <edusotelo@MacBook-Pro-de-Eduardo.local>
2025-08-16 23:01:18 -0300

    simple food domain

--- food/domain/food.go
+++ food/domain/food.go
@@ -1,1 +1,11 @@
 package domain
+
+type Food struct {
+	name string
+}
+
+func NewFood(name string) *Food {
+	return &Food{
+		name: name,
+	}
+}
---
+++ food/domain/food_test.go
@@ -0,0 +1,13 @@
+package domain
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestShouldReturnInstanceOfFood(t *testing.T) {
+	food := NewFood("foo")
+
+	assert.NotNil(t, food)
+	assert.Equal(t, food.name, "foo")
+}
